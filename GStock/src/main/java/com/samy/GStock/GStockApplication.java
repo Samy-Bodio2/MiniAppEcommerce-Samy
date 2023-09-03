c2ceb4d9d7127ca9f4baf81e51ab445719f79cce
Samy-Bodio2 <samybodio8@gmail.com>
2023-09-03 05:16:24 +0100

    first commit

---
+++ GStock/src/main/java/com/samy/GStock/GStockApplication.java
@@ -0,0 +1,13 @@
+package com.samy.GStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GStockApplication.class, args);
+	}
+
+}
---
+++ GStock/src/test/java/com/samy/GStock/GStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samy.GStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
